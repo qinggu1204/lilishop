064dc22dd533539c075fae0dd007e01191e61c31
JiaYi Li <572836873@qq.com>
2023-05-29 08:31:49 +0800

    商品接口测试2.0

--- seller-api/src/test/java/cn/lili/controller/goods/GoodsStoreControllerTest.java
+++ seller-api/src/test/java/cn/lili/controller/goods/GoodsStoreControllerTest.java
@@ -2,9 +2,12 @@ package cn.lili.controller.goods;
 
 import cn.lili.common.enums.ResultCode;
 import cn.lili.common.vo.ResultMessage;
+import cn.lili.modules.distribution.entity.dto.DistributionGoodsSearchParams;
 import cn.lili.modules.goods.entity.dos.Goods;
 import cn.lili.modules.goods.entity.dto.GoodsSearchParams;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
@@ -12,6 +15,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -19,10 +25,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @AutoConfigureMockMvc
 public class GoodsStoreControllerTest {
     @Autowired
-    private MockMvc mvc;
+    private MockMvc mockMvc;
 
     @Test
     public void testGetByPageForMember() throws Exception {
+        String accessToken = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyQ29udGV4dCI6IntcInVzZXJuYW1lXCI6XCIxMzAxMTExMTExMVwiLFwibmlja05hbWVcIjpcIuW8oOS4iVwiLFwiZmFjZVwiOlwiaHR0cHM6Ly9saWxpc2hvcC1vc3Mub3NzLWNuLWJlaWppbmcuYWxpeXVuY3MuY29tLzE1OGJmZjgzMWNmZjQ5OWE4ZDQ1YTIyNmE2ZTAyMGMyLnBuZ1wiLFwiaWRcIjpcIjEzNzY0MTc2ODQxNDAzMjY5MTJcIixcImxvbmdUZXJtXCI6ZmFsc2UsXCJyb2xlXCI6XCJTVE9SRVwiLFwic3RvcmVJZFwiOlwiMTM3NjQzMzU2NTI0NzQ3MTYxNlwiLFwiY2xlcmtJZFwiOlwiMTM3NjQzMzU2NTI0NzQ3MTYxNlwiLFwic3RvcmVOYW1lXCI6XCLlrrblrrbkuZBcIixcImlzU3VwZXJcIjp0cnVlfSIsInN1YiI6IjEzMDExMTExMTExIiwiZXhwIjoxNjg1MjQ2NjQyfQ.02mVHKu0ZEQv3RVN6lfj0d6FIJdGw0JcBmL_TltkIkY"; //这里输入你的accessToken
+        GoodsSearchParams goodsSearchParams = new GoodsSearchParams();
 
+        MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders.get("/store/goods/goods")
+                        .header("accessToken", accessToken)
+                        .param("param", goodsSearchParams.toString()))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        String result = mvcResult.getResponse().getContentAsString();
+        // 在这里对结果进行断言
+        // 创建ObjectMapper对象
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // 将JSON字符串解析为JsonNode对象
+        JsonNode jsonNode = objectMapper.readTree(result);
+
+        System.out.println(jsonNode);
+
+        // 提取result.code字段的值
+        int code = jsonNode.get("code").asInt();
+        assertEquals(ResultCode.SUCCESS.code(), code);
     }
 }
